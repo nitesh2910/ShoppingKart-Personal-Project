3add3abaa27e0a74cce5d4ebc6df126e246bbc15
nitesh2910 <goadnitesh4813@gmail.com>
2025-04-21 21:02:11 +0530

    Created Store App and created models for product table and inside admin.py of accounts created Class for AccountAdmin for list_display and list_display_links same as for store admin for list display and prepopulated_fields

--- shoppingkart/settings.py
+++ shoppingkart/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'category',
     'accounts',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -126,6 +127,11 @@ STATIC_ROOT = BASE_DIR/'static'
 STATICFILES_DIRS = [
     BASE_DIR /'shoppingkart'/'static',
 ]
+
+# media files configuration
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
