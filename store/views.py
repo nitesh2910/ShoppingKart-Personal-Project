e898ab5598a46896dce53b9f819f68ea688ab27c
nitesh2910 <goadnitesh4813@gmail.com>
2025-04-23 22:01:58 +0530

    Now Added Product price, Title, and Desc

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from ast import Try
 from unicodedata import category
 from django.shortcuts import get_object_or_404, render
 from category.models import Category
@@ -23,4 +24,12 @@ def store(request, category_slug=None):
     return render(request, 'store/store.html', context)
 
 def product_detail(request, category_slug, product_slug):
-    return render(request, 'store/product_detail.html')
+    try:
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+    
+    context = {
+        'single_product': single_product
+    }
+    return render(request, 'store/product_detail.html', context)
